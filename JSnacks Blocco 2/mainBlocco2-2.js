97beb2c03bca71f1348d7be4a98cbd3a370c4660
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 16:18:34 +0100

    Completato snack 2 blocco 2
    Completato snack 2 blocco 2

--- JSnacks Blocco 2/mainBlocco2-2.js
+++ JSnacks Blocco 2/mainBlocco2-2.js
@@ -1,3 +1,21 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista
 // di nomi e una lista di cognomi, Gatsby vuole generare una
 // falsa lista di invitati.
+
+//variabile nomi
+let nomi = ["Francesco", "Daniele", "Alessandro", "Lorenzo" ];
+//variabile cognomi
+let cognomi = ["Totti", "De rossi", "Florenzi", "Pellegrini"];
+//variabile lista vuota
+const listaFalsa = [];
+
+//ciclo for per inserire nell'array un nome e un cognome casuali per 8 volte 
+for (let index = 0; index < 8; index++) {
+    const nomeCasuale = Math.floor(Math.random()*((nomi.length - 1) - 0 + 1) + 0);
+    const cognomeCasuale = Math.floor(Math.random()*((cognomi.length - 1) - 0 + 1) + 0);
+
+    const nome = `${nomi[nomeCasuale]} ${cognomi[cognomeCasuale]}`;
+    listaFalsa.push(nome)
+};
+
+console.log(listaFalsa);
