843c4b70ca33c1d853fb051de0cc124bc37c9797
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 15:43:47 +0100

    Completato il primo blocco di snack con ES6
    Completato il primo blocco di snack con ES6

--- JSnacks Blocco 1/mainBlocco1-5.js
+++ JSnacks Blocco 1/mainBlocco1-5.js
@@ -1,2 +1,17 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array.
+
+//array vuoto
+let numeriDispari = [];
+
+//ciclo for per chiedere all'utente di inserire 6 volte un numero
+for (let index = 0; index < 6; index++) {
+    const numeroUtente = Number(prompt("inserisci un numero"));
+    // se il numero è dispari inseriscilo nell' array numeriDispari
+    if (numeroUtente % 2 !=0) {
+        numeriDispari.push(numeroUtente)
+    };
+    
+};
+console.log(numeriDispari);
+
