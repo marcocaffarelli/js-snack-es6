371c24d22111ebdb68328b1d2e5eeb2207a48460
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 19:12:01 +0100

    Completato snack 2 blocco 5
    Completato snack 2 blocco 5

--- JSnacks Blocco 5/mainBlocco5-2.js
+++ JSnacks Blocco 5/mainBlocco5-2.js
@@ -1,2 +1,7 @@
 // Scrivi una funzione che accetti una stringa come
 // argomento e la ritorni girata (es. Ciao -> oaiC)
+
+//funzione che inverte il testo
+
+const ritornoGirato = (stringa) => stringa.split("").reverse().join("");  
+console.log(ritornoGirato("ciao"))
