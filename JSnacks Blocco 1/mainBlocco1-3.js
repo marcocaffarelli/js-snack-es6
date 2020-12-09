875a1e5368b16d28b3a46e0b9fbc64ff749cbaf9
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 14:48:21 +0100

    Completato il terzo snack con il ciclo for
    Completato il terzo snack con il ciclo for

--- JSnacks Blocco 1/mainBlocco1-3.js
+++ JSnacks Blocco 1/mainBlocco1-3.js
@@ -2,3 +2,12 @@
 // numero. Il programma stampa la somma di tutti i numeri
 // inseriti. Esegui questo programma in due versioni, con il for
 // e con il while.
+
+let somma = 0;
+
+for (let index = 0; index < 5; index++) {
+    const numeroUtente =  Number(prompt("Insert a number"));
+    somma += numeroUtente;
+    
+}
+console.log(somma);
