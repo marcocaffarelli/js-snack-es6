7e720ac513c3c8d10baabeb45d6370124f196746
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 15:53:51 +0100

    Completato il primo esercizio del blocco 2 con ES6
    Completato il primo esercizio del blocco 2 con ES6

--- JSnacks Blocco 2/mainBlocco2-1.js
+++ JSnacks Blocco 2/mainBlocco2-1.js
@@ -1,2 +1,11 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari
 // stampa il numero successivo
+
+//variabile numero utente
+let numeroUtente = Number(prompt("inserisci un numero"));
+
+if (numeroUtente % 2 == 0) {
+    console.log(`il numero inserito è ${numeroUtente}`);
+} else {
+    console.log(`${numeroUtente} il numero che hai inserito è dispari, perciò stampo ${numeroUtente + 1}`);
+};
