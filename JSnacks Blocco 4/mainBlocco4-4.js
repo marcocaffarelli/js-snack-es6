72e70d2518edf694641690ce61beb33ecc3a12de
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 18:16:01 +0100

    Completato snack 4 blocco 4
    Completato snack 4 blocco 4 con il ciclo forEach

--- JSnacks Blocco 4/mainBlocco4-4.js
+++ JSnacks Blocco 4/mainBlocco4-4.js
@@ -6,7 +6,43 @@
 // Nome sarà l’unica proprietà da compilare, le altre
 // saranno tutte settate a 0.
 
+const squadre = [
+    {
+        nome : "Roma",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "Juventus",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "Bologna",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "Inter",
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : "Milan",
+        puntiFatti : 0,
+        falliSubiti : 0
+    }
+]
+//console.log(squadre);
+
 //////////////////////////////////// B ////////////////////////////////////
 
 // Generare numeri random al posto degli 0 nelle proprietà:
 // punti fatti e falli subiti
+
+//ciclo forEach
+squadre.forEach(function (item) {
+    item.puntiFatti =  Math.floor(Math.random() * 100);
+    item.falliSubiti =  Math.floor(Math.random() * 100);
+});
+console.log(squadre);
