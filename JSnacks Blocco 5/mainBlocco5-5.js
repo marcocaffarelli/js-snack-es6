33f4c3f011b0d650b8d03aec3e48edf1c66f5c02
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 20:01:11 +0100

    Completato snack 5 blocco 5
    Completato snack 5 blocco 5

--- JSnacks Blocco 5/mainBlocco5-5.js
+++ JSnacks Blocco 5/mainBlocco5-5.js
@@ -3,3 +3,14 @@
 // massimo quanto il numero di elementi dell’array).
 // La funzione ritornerà un nuovo array con i valori che
 // hanno la posizione compresa tra “a” e “b”
+
+var piccolo = 1; //a
+var grande = 4; //b
+var listaNumeri = [2, 5, 7, 9, 15];
+
+const lista = (a, b, array) => {
+    array.push(b)
+    array.unshift(a)
+    return array.slice(a, b);
+}
+console.log(lista(piccolo, grande, listaNumeri));
