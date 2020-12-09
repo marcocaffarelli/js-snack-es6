3c2fe9f782c27c1666a70e2299aebccaa3380775
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 17:47:15 +0100

    Completato snack1 blocco 4
    Completato snack1 blocco 4

--- JSnacks Blocco 4/mainBlocco4-1.js
+++ JSnacks Blocco 4/mainBlocco4-1.js
@@ -4,7 +4,17 @@
 // Nome = palla
 // Peso = 10
 
+const palla = {
+    nome : "palla",
+    peso : "10"
+};
+console.log(palla);
+
 //////////////////////////////////// B ////////////////////////////////////
 
 // Attraverso un prompt dare la possibilità all’utente di
 // modificare il peso della palla.
+
+const peso2 = prompt("inserisci un peso")
+palla['peso'] = peso2;
+console.log(palla);
