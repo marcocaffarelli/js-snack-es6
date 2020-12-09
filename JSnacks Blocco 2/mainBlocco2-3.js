72dccc445082f47f7a9e25d59f501bf3a77d59bd
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 16:58:44 +0100

    Completato snack 3 blocco 2
    Completato snack 3 blocco 2

--- JSnacks Blocco 2/mainBlocco2-3.js
+++ JSnacks Blocco 2/mainBlocco2-3.js
@@ -1,2 +1,12 @@
-// Crea un array di numeri interi e fai la somma di tutti gli
-// elementi che sono in posizione dispari
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+//variabili
+const listaNumeri = [1, 4, 2, 15, 120, 17, 3];
+let somma = 0;
+
+//ciclo for per sommare tutti gli elementi in posizione dispari
+for (let index = 1; index < listaNumeri.length; index +=2) {
+    somma += listaNumeri[index];
+    
+}
+console.log(somma); 
