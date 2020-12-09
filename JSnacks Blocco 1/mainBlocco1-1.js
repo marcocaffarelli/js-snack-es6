f49502bb292e57963dc32a3481325807519c1943
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 14:31:09 +0100

    Completato il primo snack con ES6
    Completato il primo snack con ES6

--- JSnacks Blocco 1/mainBlocco1-1.js
+++ JSnacks Blocco 1/mainBlocco1-1.js
@@ -1,2 +1,19 @@
 // L’utente inserisce due numeri in successione, con due
 // prompt. Il software stampa il maggiore.
+
+//variabile numero uno
+let numeroUno = Number(prompt("Inserisci un numero"));
+//variabile numero due
+let numeroDue = Number(prompt("Inserisci un secondo numero"));
+
+//console.log(numeroUno, numeroDue);
+
+//if per stampare il numero maggiore
+if (numeroUno  > numeroDue) {
+    console.log(`Il numero maggiore è il primo numero inserito cioè il numero ${numeroUno}`);
+} else if (numeroUno  < numeroDue) {
+    console.log(`Il numero maggiore è il secondo numero inserito cioè il numero ${numeroDue}`);
+} else {
+    console.log("I due numeri inseriti sono uguali");
+}
+
