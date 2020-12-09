f4ecd52d306c768f4cec689c422c91bf00f9b58f
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-09 17:37:36 +0100

    Completato snack 5 blocco 2 con un arrow function
    Completato snack 5 blocco 2 con un arrow function

--- JSnacks Blocco 2/mainBlocco2-5.js
+++ JSnacks Blocco 2/mainBlocco2-5.js
@@ -1,3 +1,21 @@
 // Scrivi una funzione per stabilire se un numero è primo.
 // Chiedi un numero all’utente e utilizza la funzione per
 // comunicargli se il numero inserito è primo oppure no.
+
+// Arrow function per verificare se un numero è primo
+const ctrlNumeroPrimo = numero => {
+    for(let index = 2; index < numero; index++) {
+        if(numero % index === 0) return false;
+    };
+    return numero !== 1;      
+};
+
+//variabile numero utente
+const numeroUtente = Number(prompt("inserisci un numero"));
+console.log(ctrlNumeroPrimo(numeroUtente));
+
+if (ctrlNumeroPrimo(numeroUtente) == true) {
+    console.log(`${numeroUtente} è un numero primo`);
+}else{
+    console.log(`${numeroUtente} non è un numero primo`);
+}
